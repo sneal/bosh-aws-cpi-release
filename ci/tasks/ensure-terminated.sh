23e3ac7391b3009eb414e95b28b796a241da4395
Edwin Xie <exie@pivotal.io>
2016-05-10 12:25:30 -0700

    Fixes ensure-terminated sed command.
    [#119028263](https://www.pivotaltracker.com/story/show/119028263)
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/tasks/ensure-terminated.sh
+++ ci/tasks/ensure-terminated.sh
@@ -13,7 +13,7 @@ vpc_id=$(echo ${metadata} | jq --raw-output ".VPCID")
 
 if [ ! -z "${vpc_id}" ] ; then
   instances=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId[]" --filters "Name=vpc-id,Values=${vpc_id}" | jq '.[]' --raw-output)
-  instance_list=$(echo ${instances} | sed s/[\n\r]+/ /g)
+  instance_list=$(echo ${instances} | sed "s/[\n\r]+/ /g")
 
   aws ec2 terminate-instances --instance-ids ${instance_list}
 fi
